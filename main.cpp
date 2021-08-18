9bb0413c6e45db3747b64595bbc6f20b2b8a1a93
IzyanzZ <87628633+IzyanzZ@users.noreply.github.com>
2021-08-18 08:05:28 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,5 +3,14 @@
 using namespace std;
 
 int main(){
-  cout << "C++" << endl;
+  int angka;
+  cout << "Masukan Angka Random: ";
+  cin >> angka;
+  cout << "Angka Pertama Adalah " angka << endl;
+  int angka2;
+  cout << "Masukan Angka Ke2 Random: ";
+  cin >> angka2;
+  cout << "Angka Kedua Adalah " angka2 << endl;
+  cout << angka + angka2 << endl;
+  return 0;
 }
